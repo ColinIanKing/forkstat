046904c0fb6b88e1175acaa5a2306d9f952396bb
Colin Ian King <colin.king@canonical.com>
2014-03-17 10:14:49 +0000

    Add kernel thread detection
    Print kernel threads in [ ] brackets, this code was shamelessly stolen
    from my eventstat tool.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- forkstat.c
+++ forkstat.c
@@ -62,22 +62,131 @@
 typedef struct proc_info {
 	pid_t	pid;		/* Process ID */
 	char	*cmdline;	/* /proc/pid/cmdline text */
+	bool	kernel_thread;	/* true if a kernel thread */
 	struct timeval start;	/* time when process started */
 	struct proc_info *next;	/* next proc info in hashed linked list */
 } proc_info_t;
 
+/* For kernel task checking */
+typedef struct {
+	char *task;		/* Name of kernel task */
+	size_t len;		/* Length */
+} kernel_task_info;
+
+#define KERN_TASK_INFO(str)	{ str, sizeof(str) - 1 }
+
 static bool stop_recv;				/* sighandler stop flag */
+static bool sane_procs;				/* true if not inside a container */
 static proc_info_t *proc_info[MAX_PIDS];	/* Proc hash table */
 static unsigned int opt_flags = OPT_CMD_LONG;	/* Default option */
 static int row = 0;				/* tty row number */
 
 /* Default void no process info struct */
 static proc_info_t no_info = {
-	NULL_PID,
-	"<unknown>",
-	{ 0, 0 },
-	NULL,
+	.pid = NULL_PID,
+	.cmdline = "<unknown>",
+	.kernel_thread = false,
+	.start = { 0, 0 },
+	.next = NULL,
 };
+
+/*
+ *  sane_proc_pid_info()
+ *	detect if proc info mapping from /proc/timer_stats
+ *	maps to proc pids OK. If we are in a container or
+ *	we can't tell, return false.
+ */
+static bool sane_proc_pid_info(void)
+{
+	FILE *fp;
+	static const char pattern[] = "container=";
+	const char *ptr = pattern;
+	bool ret = true;
+
+	fp = fopen("/proc/1/environ", "r");
+	if (!fp)
+		return false;
+
+	while (!feof(fp)) {
+		int ch = getc(fp);
+
+		if (*ptr == ch) {
+			ptr++;
+			/* Match? So we're inside a container */
+			if (*ptr == '\0') {
+				ret = false;
+				break;
+			}
+		} else {
+			/* No match, skip to end of var and restart scan */
+			do {
+				ch = getc(fp);
+			} while ((ch != EOF) && (ch != '\0'));
+			ptr = pattern;
+		}
+	}
+
+	fclose(fp);
+
+	return ret;
+}
+
+/*
+ *  pid_a_kernel_thread
+ *
+ */
+static bool pid_a_kernel_thread(const char *task, const pid_t id)
+{
+	if (sane_procs) {
+		return getpgid(id) == 0;
+	} else {
+		/* In side a container, make a guess at kernel threads */
+		int i;
+		pid_t pgid = getpgid(id);
+
+		/* This fails for kernel threads inside a container */
+		if (pgid >= 0)
+			return pgid == 0;
+
+		/*
+		 * This is not exactly accurate, but if we can't look up
+		 * a process then try and infer something from the comm field.
+		 * Until we have better kernel support to map /proc/timer_stats
+		 * pids to containerised pids this is the best we can do.
+		 */
+		static kernel_task_info kernel_tasks[] = {
+			KERN_TASK_INFO("swapper/"),
+			KERN_TASK_INFO("kworker/"),
+			KERN_TASK_INFO("ksoftirqd/"),
+			KERN_TASK_INFO("watchdog/"),
+			KERN_TASK_INFO("migration/"),
+			KERN_TASK_INFO("irq/"),
+			KERN_TASK_INFO("mmcqd/"),
+			KERN_TASK_INFO("jbd2/"),
+			KERN_TASK_INFO("kthreadd"),
+			KERN_TASK_INFO("kthrotld"),
+			KERN_TASK_INFO("kswapd"),
+			KERN_TASK_INFO("ecryptfs-kthrea"),
+			KERN_TASK_INFO("kauditd"),
+			KERN_TASK_INFO("kblockd"),
+			KERN_TASK_INFO("kcryptd"),
+			KERN_TASK_INFO("kdevtmpfs"),
+			KERN_TASK_INFO("khelper"),
+			KERN_TASK_INFO("khubd"),
+			KERN_TASK_INFO("khugepaged"),
+			KERN_TASK_INFO("khungtaskd"),
+			KERN_TASK_INFO("flush-"),
+			KERN_TASK_INFO("bdi-default-"),
+			{ NULL, 0 }
+		};
+
+		for (i = 0; kernel_tasks[i].task != NULL; i++) {
+			if (strncmp(task, kernel_tasks[i].task, kernel_tasks[i].len) == 0)
+				return true;
+		}
+	}
+	return false;
+}
 
 /*
  *  tty_height()
@@ -320,6 +429,7 @@ static proc_info_t *proc_info_add(const pid_t pid, struct timeval *tv)
 	}
 	info->cmdline = cmdline;
 	info->pid = pid;
+	info->kernel_thread = pid_a_kernel_thread(cmdline, pid);
 
 	if (tv)
 		info->start = *tv;
@@ -516,24 +626,33 @@ static int monitor(const int sock)
 				info2 = proc_info_add(proc_ev->event_data.fork.child_pid, &tv);
 				if (info1 != NULL && info2 != NULL) {
 					row_increment();
-					printf("%s fork %5d parent %8s %s\n",
+					printf("%s fork %5d parent %8s %s%s%s\n",
 						when,
 						proc_ev->event_data.fork.parent_pid,
-						"", info1->cmdline);
+						"",
+						info1->kernel_thread ? "[" : "",
+						info1->cmdline,
+						info1->kernel_thread ? "]" : "");
 					row_increment();
-					printf("%s fork %5d child  %8s %s\n",
+					printf("%s fork %5d child  %8s %s%s%s\n",
 						when,
 						proc_ev->event_data.fork.child_pid,
-						"", info2->cmdline);
+						"",
+						info1->kernel_thread ? "[" : "",
+						info2->cmdline,
+						info1->kernel_thread ? "]" : "");
 				}
 				break;
 			case PROC_EVENT_EXEC:
 				info1 = proc_info_update(proc_ev->event_data.exec.process_pid);
 				row_increment();
-				printf("%s exec %5d        %8s %s\n",
+				printf("%s exec %5d        %8s %s%s%s\n",
 					when,
-				proc_ev->event_data.exec.process_pid,
-					"", info1->cmdline);
+					proc_ev->event_data.exec.process_pid,
+					"",
+					info1->kernel_thread ? "[" : "",
+					info1->cmdline,
+					info1->kernel_thread ? "]" : "");
 				break;
 			case PROC_EVENT_EXIT:
 				info1 = proc_info_get(proc_ev->event_data.exec.process_pid);
@@ -548,12 +667,14 @@ static int monitor(const int sock)
 					snprintf(duration, sizeof(duration), "unknown");
 				}
 				row_increment();
-				printf("%s exit %5d  %5d %8s %s\n",
+				printf("%s exit %5d  %5d %8s %s%s%s\n",
 					when,
 					proc_ev->event_data.exit.process_pid,
 					proc_ev->event_data.exit.exit_code,
-					duration, info1->cmdline);
-
+					duration,
+					info1->kernel_thread ? "[" : "",
+					info1->cmdline,
+					info1->kernel_thread ? "]" : "");
 				if (proc_ev->event_data.exit.process_pid ==
 					   proc_ev->event_data.exit.process_tgid)
 					proc_info_free(proc_ev->event_data.exit.process_pid);
@@ -611,6 +732,8 @@ int main(int argc, char * const argv[])
 		fprintf(stderr, "Need to run with root access.\n");
 		goto abort_sock;
 	}
+
+	sane_procs = sane_proc_pid_info();
 
 	if (proc_info_load() < 0) {
 		fprintf(stderr, "Cannot load process cache. Is /proc mounted?\n");
