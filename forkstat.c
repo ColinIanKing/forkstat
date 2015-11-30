ColinIanKing/forkstat

commit 476c9833b4d0169aecf315599144352daab99728

Use more efficient hash function
Signed-off-by: Colin Ian King <colin.king@canonical.com>

forkstat.c
@@ -416,25 +416,21 @@ static inline int proc_info_hash(const pid_t pid)
 	return pid % MAX_PIDS;
 }
 
+
 /*
  *  proc_name_hash()
- *	hash on proc name, from Aho, Sethi, Ullman, Compiling Techniques.
+ *	Hash a string, from Dan Bernstein comp.lang.c (xor version)
  */
 static inline int proc_name_hash(const char *str)
 {
-	unsigned long h = 0;
+	register int hash = 5381;
+	register int c;
 
-	while (*str) {
-		unsigned long g;
-		h = (h << 4) + (*str);
-		if (0 != (g = h & 0xf0000000)) {
-			h = h ^ (g >> 24);
-			h = h ^ g;
-		}
-		str++;
+	while ((c = *str++)) {
+		/* (hash * 33) ^ c */
+		hash = ((hash << 5) + hash) ^ c;
 	}
-
-	return h % MAX_PIDS;
+	return hash % MAX_PIDS;
 }
 
 static void proc_stats_account(const pid_t pid, const event_t event)

commit ce8b34eb39432ff3b9b82791fccee1f595834e87

ensure hash is unsigned, otherwise we get -ve hash
Fixes: 476c9833b4d01 ("Use more efficient hash function")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

forkstat.c
@@ -421,10 +421,10 @@ static inline int proc_info_hash(const pid_t pid)
  *  proc_name_hash()
  *	Hash a string, from Dan Bernstein comp.lang.c (xor version)
  */
-static inline int proc_name_hash(const char *str)
+static inline unsigned int proc_name_hash(const char *str)
 {
-	register int hash = 5381;
-	register int c;
+	register unsigned int hash = 5381;
+	register unsigned int c;
 
 	while ((c = *str++)) {
 		/* (hash * 33) ^ c */
@@ -435,7 +435,7 @@ static inline int proc_name_hash(const char *str)
 
 static void proc_stats_account(const pid_t pid, const event_t event)
 {
-	int h;
+	unsigned int h;
 	char *name;
 	proc_stats_t *stats;
 	proc_info_t *info;