ColinIanKing/forkstat

commit 823585a3b916c633c991739097c88f3c0a71afb3

Use strtol rather than atoi to avoid any number parsing overflows or errors
Signed-off-by: Colin Ian King <colin.king@canonical.com>

forkstat.c
@@ -645,8 +645,11 @@ static void proc_thread_info_add(pid_t pid)
 
 	while ((dirent = readdir(dir))) {
 		if (isdigit(dirent->d_name[0])) {
-			pid_t tpid = atoi(dirent->d_name);
-			if (tpid != pid)
+			pid_t tpid;
+
+			errno = 0;
+			tpid = (pid_t)strtol(dirent->d_name, NULL, 10);
+			if ((!errno) && (tpid != pid))
 				(void)proc_info_add(tpid, NULL);
 		}
 	}
@@ -668,9 +671,13 @@ static int proc_info_load(void)
 
 	while ((dirent = readdir(dir))) {
 		if (isdigit(dirent->d_name[0])) {
-			pid_t pid = atoi(dirent->d_name);
+			pid_t pid;
+
+			errno = 0;
+			pid = (pid_t)strtol(dirent->d_name, NULL, 10);
 			(void)proc_info_add(pid, NULL);
-			proc_thread_info_add(pid);
+			if (!errno)
+				proc_thread_info_add(pid);
 		}
 	}
 

commit d5c6d176df16f652eeeaca19590aae16e543e18b

Only add proc_info if strtol parses pid correctly.
Somehow I botched the original check and forgot to also include
proc_info_add into the check if errno is OK.

Fixes: 823585a3b916c633c991 ("Use strtol rather than atoi to avoid any number parsing overflows or errors")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

forkstat.c
@@ -849,9 +849,10 @@ static int proc_info_load(void)
 
 			errno = 0;
 			pid = (pid_t)strtol(dirent->d_name, NULL, 10);
-			(void)proc_info_add(pid, NULL);
-			if (!errno)
+			if (!errno) {
+				(void)proc_info_add(pid, NULL);
 				proc_thread_info_add(pid);
+			}
 		}
 	}
 